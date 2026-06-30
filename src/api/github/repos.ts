8047c152ca552d77a8503e25302ed4cc9217846f
Marlon Marcello <marlon.marcello@gmail.com>
2026-06-29 17:43:36 -0700

    chore: linting

--- src/api/github/repos.ts
+++ src/api/github/repos.ts
@@ -257,7 +257,10 @@ export async function createGitHubRepo(input: CreateGitHubRepoInput): Promise<Cr
 export async function createGitHubRepoWithSetup(
   input: CreateGitHubRepoWithSetupInput,
 ): Promise<CreateGitHubRepoWithSetupResult> {
-  if ((input.templateOwner && !input.templateRepo) || (!input.templateOwner && input.templateRepo)) {
+  if (
+    (input.templateOwner && !input.templateRepo) ||
+    (!input.templateOwner && input.templateRepo)
+  ) {
     throw new Error("Both templateOwner and templateRepo must be provided together.");
   }
 
